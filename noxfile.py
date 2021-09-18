0d49e09a457ed7cb460e88dd58fcee13918ea88f
Krzysztof Czarnecki <krzysztof.czarnecki@technica-engineering.de>
2021-09-18 05:01:50 +0200

    feat: implement basic `noxfile`

--- noxfile.py
+++ noxfile.py
@@ -1,32 +1,63 @@
 import nox
+import os
+import shutil
 
 
 @nox.session()
 def test(session: nox.Session):
-    pass
+    """Run tests under coverage"""
+    session.install(
+        '.',
+        'coverage'
+    )
+    session.run(
+        'coverage', 'run', '-m', 'unittest', 'discover'
+    )
+
+
+@nox.session()
+def cov(session: nox.Session):
+    """Compile coverage to an XML file"""
+    session.install('coverage')
+    session.run('coverage', 'xml', '-i')
 
 
 @nox.session
 def lint(session: nox.Session):
+    """Run the linter"""
     session.install('flake8')
-    session.run()
+    session.run('flake8')
 
 
 @nox.session()
-def docs(session: nox.Session):
-    pass
-
-
-@nox.session()
-def wheel(session: nox.Session):
-    pass
+def whl(session: nox.Session):
+    """Build the wheel"""
+    session.install(
+        'wheel',
+        'setuptools',
+        '.'
+    )
+    session.run('python', '-m', 'setup', 'bdist_wheel')
 
 
 @nox.session()
 def send(session: nox.Session):
-    pass
+    """Send wheel to PyPi"""
+    session.install('twine')
+    session.run('twine', 'upload', 'dist/*.whl')
 
 
 @nox.session()
 def clean(session: nox.Session):
-    pass
+    """Remove files that aren't needed anymore"""
+    delete = [
+        ".coverage",
+        "build",
+        "dist"
+    ]
+    for p in delete:
+        if os.path.exists(p):
+            if os.path.isdir(p):
+                shutil.rmtree(p)
+            else:
+                os.remove(p)
