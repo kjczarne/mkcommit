b10f4bd6ea96836f0c46647290130810c994dfe7
Krzysztof Czarnecki <kjczarnecki@icloud.com>
2021-09-21 00:18:15 +0200

    fix: wrong keyword list used in `CommaSeparatedList`

--- test/res/example.commas.mkcommit.py
+++ test/res/example.commas.mkcommit.py
@@ -6,7 +6,7 @@ keywords = [
 ]
 
 selected_keywords = ask("Select keywords", one_or_more=keywords)
-selected_keywords_str = CommaSeparatedList(*[k.keyword for k in keywords])
+selected_keywords_str = CommaSeparatedList(*[k.keyword for k in selected_keywords])
 short_commit_message = ask("Short commit message")
 
 c = CommitMessage(
