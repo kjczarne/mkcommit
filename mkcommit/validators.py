0c9ae76c83fad64442c623df9ac13731d0fa3067
Krzysztof Czarnecki <krzysztof.czarnecki@technica-engineering.de>
2021-09-20 14:11:25 +0200

    fix: validators should match entire string

--- mkcommit/validators.py
+++ mkcommit/validators.py
@@ -4,6 +4,7 @@ import re
 
 def matches(pattern: str) -> Validator:
     def _v(msg: str) -> bool:
+        """Checks if the input matches a RegEx pattern"""
         if re.match(pattern, msg):
             return True
         else:
@@ -13,18 +14,21 @@ def matches(pattern: str) -> Validator:
 
 def is_int() -> Validator:
     def _v(msg: str) -> bool:
-        return matches(r'\d+')(msg)
+        """Checks if the input is an integer"""
+        return matches(r'^\d+$')(msg)
     return _v
 
 
 def is_float() -> Validator:
     def _v(msg: str) -> bool:
-        return matches(r'\d+\.\d+|\d+')(msg)
+        """Checks if the input is a float"""
+        return matches(r'^\d+\.\d+$|^\d+$')(msg)
     return _v
 
 
 def max_len(limit: int) -> Validator:
     def _v(msg: str) -> bool:
+        """Checks if the input exceeds maximum length"""
         if len(msg) > limit:
             return False
         else:
