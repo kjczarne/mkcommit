e2a34771a64bd0018851dc89a45d7f9590766033
Krzysztof Czarnecki <kjczarnecki@icloud.com>
2021-09-25 01:41:07 +0200

    refactor: use `editor` instead of explicit `vscode`

--- test/res/example.editor.mkcommit.py
+++ test/res/example.editor.mkcommit.py
@@ -1,6 +1,6 @@
-from mkcommit import CommitMessage, vscode, to_stdout
+from mkcommit import CommitMessage, editor, to_stdout
 
-c = CommitMessage("whatever", vscode())
+c = CommitMessage("whatever", editor())
 
 if __name__ == "__main__":
     to_stdout(c)
