e7eeefce04bc65d532ace10b9256ce842ea73d1d
Krzysztof Czarnecki <kjczarnecki@icloud.com>
2021-09-26 02:23:33 +0200

    chore: refactor `.mkcommit.py` to comply with new pattern

--- .mkcommit.py
+++ .mkcommit.py
@@ -2,13 +2,18 @@ from mkcommit import CommitMessage, to_stdout, ask
 from mkcommit.suites import semantic
 from mkcommit.validators import is_true
 
-ask("Have you documented your changes?", yes_no=True, check=is_true())
-ask("Have you updated the validators list?", yes_no=True, check=is_true())
-ask("Have you checked whether the tests are passing?", yes_no=True, check=is_true())
+
+def commit():
+    ask("Have you documented your changes?", yes_no=True, check=is_true())
+    ask("Have you updated the validators list?", yes_no=True, check=is_true())
+    ask("Have you checked whether the tests are passing?", yes_no=True, check=is_true())
+
+    return CommitMessage(semantic.default_short())
 
 
-c = CommitMessage(semantic.default_short())
+def pre_commit(msg: CommitMessage):
+    pass  # TODO: semantic doesn't declare a validator yet
 
 
 if __name__ == "__main__":
-    to_stdout(c)
+    to_stdout(commit())
