a7f2d1d662a584c2b14d459863fc60e2f06330d4
Krzysztof Czarnecki <krzysztof.czarnecki@technica-engineering.de>
2021-10-29 20:02:35 +0200

    refactor: use `conventional` suite for this project

--- .mkcommit.py
+++ .mkcommit.py
@@ -1,5 +1,5 @@
 from mkcommit import CommitMessage, to_stdout, ask
-from mkcommit.suites import semantic
+from mkcommit.suites import conventional
 from mkcommit.validators import is_true
 
 
@@ -8,12 +8,11 @@ def commit():
     ask("Have you updated the validators list?", yes_no=True, check=is_true())
     ask("Have you checked whether the tests are passing?", yes_no=True, check=is_true())
 
-    return CommitMessage(semantic.default_short())
+    return CommitMessage(*conventional.default())
 
 
 def on_commit(msg: CommitMessage):
-    semantic.is_semantic(msg.first_line)
-    semantic.has_short_commit_msg_proper_length(msg.first_line)
+    conventional.is_conventional(msg.first_line)
 
 
 if __name__ == "__main__":
